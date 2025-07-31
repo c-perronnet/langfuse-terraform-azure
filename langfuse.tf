ddf2f981b0e6cbaab131aad68b8433f236d05a11
Steffen Schmitz <steffen@langfuse.com>
2025-07-31 10:28:43 +0200

    feat: allow additionalEnv settings in module (#18)

--- langfuse.tf
+++ langfuse.tf
@@ -87,6 +87,29 @@ langfuse:
   - name: LANGFUSE_USE_AZURE_BLOB
     value: "true"
 EOT
+  additional_env_values = length(var.additional_env) == 0 ? "" : <<EOT
+langfuse:
+  additionalEnv:
+%{for env in var.additional_env}
+  - name: ${env.name}
+%{if env.value != null}
+    value: "${env.value}"
+%{endif}
+%{if env.valueFrom != null}
+    valueFrom:
+%{if env.valueFrom.secretKeyRef != null}
+      secretKeyRef:
+        name: ${env.valueFrom.secretKeyRef.name}
+        key: ${env.valueFrom.secretKeyRef.key}
+%{endif}
+%{if env.valueFrom.configMapKeyRef != null}
+      configMapKeyRef:
+        name: ${env.valueFrom.configMapKeyRef.name}
+        key: ${env.valueFrom.configMapKeyRef.key}
+%{endif}
+%{endif}
+%{endfor}
+EOT
 }
 
 resource "kubernetes_namespace" "langfuse" {
@@ -142,6 +165,7 @@ resource "helm_release" "langfuse" {
     local.langfuse_values,
     local.ingress_values,
     local.encryption_values,
-    local.sso_values
+    local.sso_values,
+    local.additional_env_values
   ]
 }
