e1081b845bbbf876d861d754ae2145646107b890
c-perronnet <159153275+c-perronnet@users.noreply.github.com>
2025-10-02 09:51:38 +0200

    Update network.tf

--- network.tf
+++ network.tf
@@ -11,6 +11,20 @@ resource "azurerm_virtual_network" "this" {
     }
   }
 }
+output "virtual_network_id" {
+  description = "The ID of the virtual network"
+  value       = azurerm_virtual_network.this.id
+}
+
+output "virtual_network_name" {
+  description = "The name of the virtual network"
+  value       = azurerm_virtual_network.this.name
+}
+
+output "virtual_network_address_space" {
+  description = "The address space of the virtual network"
+  value       = azurerm_virtual_network.this.address_space
+}
 
 # Add DDoS Protection Plan
 resource "azurerm_network_ddos_protection_plan" "this" {
