fd1a1878d3ecf1fccc0e0dc6da68aedf8b27c240
Cedric Perronnet <c.perronnet@unesco.org>
2025-10-03 14:12:30 +0200

    additional env

--- langfuse.tf
+++ langfuse.tf
@@ -59,6 +59,31 @@ langfuse:
       name: ${kubernetes_secret.langfuse.metadata[0].name}
       key: encryption-key
   additionalEnv:
+  - name: LANGFUSE_USE_AZURE_BLOB
+    value: "true" 
+EOT
+  additional_env_values = length(var.additional_env) == 0 ? "" : <<EOT
+langfuse:
+  additionalEnv:
+%{for env in var.additional_env}
+  - name: ${env.name}
+%{if env.value != null}
+    value: "${env.value}"
+%{endif}
+%{if env.valueFrom != null}
+    valueFrom:
+%{if env.valueFrom.secretKeyRef != null}
+      secretKeyRef:
+        name: ${env.valueFrom.secretKeyRef.name}
+        key: ${env.valueFrom.secretKeyRef.key}
+%{endif}
+%{if env.valueFrom.configMapKeyRef != null}
+      configMapKeyRef:
+        name: ${env.valueFrom.configMapKeyRef.name}
+        key: ${env.valueFrom.configMapKeyRef.key}
+%{endif}
+%{endif}
+%{endfor}
 EOT
 }
 
